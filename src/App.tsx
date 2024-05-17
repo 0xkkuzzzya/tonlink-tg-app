fdf9771f2a0d83ac61094e68afb8682ac4914447
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-05-17 19:08:19 +0500

    fix:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { MainIndex } from './components';
+import styled from 'styled-components';
+import mainbg from './assets/main-bg.jpg'
+
+const Container = styled.div`
+	background-image: url(${mainbg});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-attachment: fixed;
+`
 
 
 function App() {
 	return (
-		<div className="App">
+		<Container>
 			<MainIndex/>
-		</div>
+		</Container>
 	);
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,17 +2,12 @@ import styled from "styled-components";
 import { Routes, Route } from 'react-router-dom';
 import { Header } from "./Header/Header";
 import { MainPage } from "./Pages/Main";
-import mainbg from '../assets/main-bg.jpg'
+
 
 const Container = styled.div`
     width: 390px;
-    height: 100%;
+    height: 100vh;
     margin: 0 auto;
-    padding-bottom: 50px;
-    background-image: url(${mainbg});
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-attachment: fixed;
     @media (max-width: 390px) {
         width: 100%;
     }
